b3fbe98f9f9eca1722fc817de871bd429538fb36
narasimhacha <laxminarasimhachary4@gmail.com>
2025-12-25 23:52:54 +0530

    Here is my first Dsa problem of reversing a string

---
+++ reversing a str using two pointers.py
@@ -0,0 +1,13 @@
+def reverseString(s):
+    left, right = 0, len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+
+
+# driver code
+if __name__ == "__main__":
+    s = ["h", "e", "l", "l", "o"]
+    reverseString(s)
+    print(s)
